1020a4dd13f494830713ae896af0a0e5d2f1f689
m <scheer@freescience.de>
2018-09-29 19:33:28 +0200

    style package

--- R/init.R
+++ R/init.R
@@ -1,13 +1,13 @@
-.onAttach <- function(libname, pkgname){
+.onAttach <- function(libname, pkgname) {
   packageStartupMessage("Package projectPlan leverage the futile.logger-package. Use futile.logger::flog.threshold() to modify the visibility of messages.")
 }
 
-.onLoad <- function(libname, pkgname){
-
+.onLoad <- function(libname, pkgname) {
   packageStartupMessage("Package projectPlan leverage the futile.logger-package. Use futile.logger::flog.threshold() to modify the visibility of messages.")
 
   futile.logger::flog.layout(
-    futile.logger::layout.format('[~l] [~t] [~n.~f] ~m'),
-    name = pkgname)
+    futile.logger::layout.format("[~l] [~t] [~n.~f] ~m"),
+    name = pkgname
+  )
   futile.logger::flog.threshold(futile.logger::WARN, name = pkgname)
 }
