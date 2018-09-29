36102d12dbfd49980174cbbbaa44ac012956c1b5
m <scheer@freescience.de>
2018-09-29 18:08:53 +0200

    init

---
+++ R/h_wrangle.R
@@ -0,0 +1,22 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' Calls all necessary for wrangling a raw project plan
+#'
+#' @return No direct return value, i.e. call by reference
+#'
+#' @examples
+h.wrangle_raw_data <- function(dt_ref) {
+}
---
+++ R/init.R
@@ -0,0 +1,13 @@
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("Package projectPlan leverage the futile.logger-package. Use futile.logger::flog.threshold() to modify the visibility of messages.")
+}
+
+.onLoad <- function(libname, pkgname){
+
+  packageStartupMessage("Package projectPlan leverage the futile.logger-package. Use futile.logger::flog.threshold() to modify the visibility of messages.")
+
+  futile.logger::flog.layout(
+    futile.logger::layout.format('[~l] [~t] [~n.~f] ~m'),
+    name = pkgname)
+  futile.logger::flog.threshold(futile.logger::WARN, name = pkgname)
+}
