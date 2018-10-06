7b4c998cda37380a7bdd52c773beca97041489e6
m <scheer@freescience.de>
2018-10-06 09:56:43 +0200

    sheet name is used if project column is not found

--- R/h.import.R
+++ R/h.import.R
@@ -11,14 +11,19 @@ import_xlsx <- function(fName, sheets) {
       function(s) {
         futile.logger::flog.info(glue::glue("Import {fName} - {s}"))
          df <- readxl::read_xlsx(fName, sheet = s, skip = 0)
+         if (nrow(df) == 0) {
+           futile.logger::flog.info(glue::glue("Skip the empty sheet -{s}"))
+           return(NULL)
+         }
          if (!("project" %in% names(dt))) {
            empty_rows <- with(df, is.na(section) & is.na(id) & is.na(start) & is.na(end) & is.na(resource) & is.na(task))
            futile.logger::flog.info(glue::glue("No project column found. Create one using the sheetname -{s}"))
            df$project <- NA_character_
-           df$project[!idx] <- s
+           df$project[!empty_rows] <- s
          }
+         df
       }
     )
-  
+  #browser()
   data.table::rbindlist(dt_list, fill = TRUE)
 }
