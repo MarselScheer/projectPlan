bf7ab8636eb74dbe22f251b2aebff122a2928009
m <scheer@freescience.de>
2018-10-06 09:56:03 +0200

    sheet name is used if no column project is found

--- R/h.import.R
+++ R/h.import.R
@@ -1,26 +1,22 @@
 #' @export
-import_xlsx <- function(fName, sheets, prefix) {
+import_xlsx <- function(fName, sheets) {
   futile.logger::flog.info(glue::glue("Import {fName}."))
   
   if (missing(sheets)) {
     sheets <- readxl::excel_sheets(fName)
-  }
-  if (missing(prefix)) {
-    prefix <- sheets
-  }
-  
-  if (length(sheets) != length(prefix)) {
-    msg <- "sheets and prefix must have the same length"
-    futile.logger::flog.error(msg)
-    stop(msg)
   }
   
   dt_list <-
     lapply(sheets,
       function(s) {
         futile.logger::flog.info(glue::glue("Import {fName} - {s}"))
-         readxl::read_xlsx(fName, sheet = s, skip = 0)
-        
+         df <- readxl::read_xlsx(fName, sheet = s, skip = 0)
+         if (!("project" %in% names(dt))) {
+           empty_rows <- with(df, is.na(section) & is.na(id) & is.na(start) & is.na(end) & is.na(resource) & is.na(task))
+           futile.logger::flog.info(glue::glue("No project column found. Create one using the sheetname -{s}"))
+           df$project <- NA_character_
+           df$project[!idx] <- s
+         }
       }
     )
   
