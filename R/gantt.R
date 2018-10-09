0165ff5fa7a49be3c9a8c5020b4577a5833b9ee4
m <scheer@freescience.de>
2018-10-09 21:05:58 +0200

    no-visible-binding-warnings addressed

--- R/gantt.R
+++ R/gantt.R
@@ -1,5 +1,6 @@
 #' @export
 gantt_by_sections <- function(dt, xlim, show_dependencies = FALSE, text_size = 3) {
+  
   xmin <- min(dt$time_start, na.rm = TRUE)
   xmax <- max(dt$time_end, na.rm = TRUE)
 
@@ -10,16 +11,17 @@ gantt_by_sections <- function(dt, xlim, show_dependencies = FALSE, text_size = 3
   }
 
   pf <- data.table::copy(dt)
-  data.table::setorder(pf, section, time_start)
+  data.table::setorder(pf, c("section", "time_start"))
   pf[, y := .N:1]
-  pf[, ":="(mean_y = mean(y), min_y = min(y), max_y = max(y)), by = "section"]
-
-  prjf <- pf[, .(min_y = min(y), max_y = max(y)), by = "project"]
-
+  y <- "y"
+  pf[, ":="(mean_y = mean(.SD), min_y = min(.SD), max_y = max(.SD)), by = "section", .SDcols = y]
+   
+  prjf <- pf[, data.table::.(min_y = min(.SD), max_y = max(.SD)), by = "project", .SDcols = y]
+  
   ret <- h.create_gantt(pf, xlim, xmin, xmax, text_size = text_size) +
-    geom_rect(aes(fill = resource)) +
-    scale_y_continuous(breaks = pf$mean_y, labels = pf$section) +
-    geom_rect(data = prjf, aes(xmin = max(xmin, xlim[1]), xmax = xmax, ymin = min_y - 0.4, ymax = max_y + 0.4), color = "blue", alpha = 0, size = 1.25) +
+    ggplot2::geom_rect(ggplot2::aes_string(fill = "resource")) +
+    ggplot2::scale_y_continuous(breaks = pf$mean_y, labels = pf$section) +
+    ggplot2::geom_rect(data = prjf, ggplot2::aes_string(xmin = max(xmin, xlim[1]), xmax = xmax, ymin = "min_y" - 0.4, ymax = "max_y" + 0.4), color = "blue", alpha = 0, size = 1.25) +
     h.geom_progress() +
     h.mark_completed_tasks(pf)
 
@@ -36,12 +38,12 @@ gantt_by_sections <- function(dt, xlim, show_dependencies = FALSE, text_size = 3
 h.calculate_arrows <- function(sorted_dt, start, end) {
   one_row_dep <- h.one_row_for_every_dependency(sorted_dt)
 
-  one_row_dep %>%
-    dplyr::left_join(dplyr::select(sorted_dt, grp, time_start, y), by = "grp") %>%
-    dplyr::rename(time_start_grp = time_start, y_grp = y) %>%
-    dplyr::left_join(dplyr::select(sorted_dt, grp, time_end, y), by = c("dep" = "grp")) %>%
-    dplyr::rename(time_end_dep = time_end, y_dep = y) %>%
-    dplyr::mutate(time_start = start, time_end = end, y = 0)
+  # one_row_dep %>%
+  #   dplyr::left_join(dplyr::select(sorted_dt, grp, time_start, y), by = "grp") %>%
+  #   dplyr::rename(time_start_grp = time_start, y_grp = y) %>%
+  #   dplyr::left_join(dplyr::select(sorted_dt, grp, time_end, y), by = c("dep" = "grp")) %>%
+  #   dplyr::rename(time_end_dep = time_end, y_dep = y) %>%
+  #   dplyr::mutate(time_start = start, time_end = end, y = 0)
 }
 
 
@@ -69,29 +71,28 @@ h.make_weekend_rows <- function(start, end) {
 
 h.create_gantt <- function(pf, xlim, xmin, xmax, text_size = 3) {
   we <- h.make_weekend_rows(xmin - 7, xmax + 7)
-
-  pf %>%
-    ggplot(aes(xmin = time_start, xmax = time_end, ymin = y - 0.3, ymax = y + 0.3)) +
-    geom_text(aes(x = time_end, y = y, label = task, hjust = 0), size = text_size) +
-    geom_rect(aes(xmin = max(xmin, xlim[1]), xmax = xmax, ymin = min_y - 0.4, ymax = max_y + 0.4), color = "black", alpha = 0, linetype = 3) +
-    geom_vline(xintercept = lubridate::as_date(lubridate::now()), size = 2, color = "red", alpha = 0.2) +
-    geom_rect(data = we, aes(xmin = time_start, xmax = time_end + 1, ymin = 0, ymax = nrow(pf)), alpha = 0.05) +
-    coord_cartesian(xlim = xlim) +
-    theme_minimal() +
-    theme(
-      panel.grid.minor.y = element_blank(),
-      panel.grid.major.y = element_blank()
+  
+  ggplot2::ggplot(pf, ggplot2::aes_string(xmin = "time_start", xmax = "time_end", ymin = "y" - 0.3, ymax = "y" + 0.3)) +
+    ggplot2::geom_text(ggplot2::aes_string(x = "time_end", y = "y", label = "task", hjust = 0), size = text_size) +
+    ggplot2::geom_rect(ggplot2::aes_string(xmin = max(xmin, xlim[1]), xmax = xmax, ymin = "min_y" - 0.4, ymax = "max_y" + 0.4), color = "black", alpha = 0, linetype = 3) +
+    ggplot2::geom_vline(xintercept = lubridate::as_date(lubridate::now()), size = 2, color = "red", alpha = 0.2) +
+    ggplot2::geom_rect(data = we, ggplot2::aes_string(xmin = "time_start", xmax = "time_end" + 1, ymin = 0, ymax = nrow(pf)), alpha = 0.05) +
+    ggplot2::coord_cartesian(xlim = xlim) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      panel.grid.minor.y = ggplot2::element_blank(),
+      panel.grid.major.y = ggplot2::element_blank()
     ) +
-    ylab("") +
-    xlab("")
+    ggplot2::ylab("") +
+    ggplot2::xlab("")
 }
 
 
 h.geom_progress <- function() {
-  geom_errorbarh(aes(xmin = time_start, xmax = time_start + (time_end - time_start) * progress / 100, y = y), size = 1)
+  ggplot2::geom_errorbarh(ggplot2::aes_string(xmin = "time_start", xmax = "time_start" + ("time_end" - "time_start") * "progress" / 100, y = "y"), size = 1)
 }
 
 h.mark_completed_tasks <- function(pf) {
   sub <- filter(pf, progress == 100)
-  geom_rect(data = sub, fill = "grey")
+  ggplot2::geom_rect(data = sub, fill = "grey")
 }
