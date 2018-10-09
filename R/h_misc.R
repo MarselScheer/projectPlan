7d471c32e4900321489be6174943a2aa6244eda0
m <scheer@freescience.de>
2018-10-09 19:21:27 +0200

    pipe operator removed

--- R/h_misc.R
+++ R/h_misc.R
@@ -10,16 +10,12 @@ h.comma_list <- function(v) {
 }
 
 h.split_comma_list <- function(str) {
-  str %>%
-    stringr::str_split_fixed(pattern = ",", n = Inf) %>%
-    unlist() %>%
-    stringr::str_trim()
+  str <- stringr::str_split_fixed(str, pattern = ",", n = Inf)
+  stringr::str_trim(unlist(str))
 }
 
 h.comma_list_2_uniq_vec <- function(str) {
-  str %>%
-    h.split_comma_list() %>%
-    unique()
+  unique(h.split_comma_list(str))
 }
 
 h.combine_comma_list_cols <- function(v, w = "") {
