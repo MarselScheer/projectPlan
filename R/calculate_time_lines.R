c36134fac44bfdb50a51cbb27b11f22202b50009
m <scheer@freescience.de>
2019-01-18 21:13:24 +0100

    overdue deadlines are generate a warning only if progress is not already 100

--- R/calculate_time_lines.R
+++ R/calculate_time_lines.R
@@ -41,11 +41,11 @@ h.calc_dist_to_deadline <- function(date_vec, deadline_vec) {
 
 
 h.calc_end_to_deadline <- function(df) {
-  idx <- !is.na(df$deadline)
+  idx <- !is.na(df$deadline) 
 
   if (any(idx)) {
     with(NULL, df[idx, dist_end_to_deadline := h.calc_dist_to_deadline(time_end, deadline)])
-    h.log_rows(df, df$dist_end_to_deadline <= 0, "DEADLINE TODAY OR ALREADY UNMET", warn_columns = c("project", "section", "id", "time_start", "time_end", "deadline", "progress", "resource", "task"))
+    h.log_rows(df, df$dist_end_to_deadline <= 0 & df$progress != 100, "DEADLINE TODAY OR ALREADY UNMET", warn_columns = c("project", "section", "id", "time_start", "time_end", "deadline", "progress", "resource", "task"))
   } else {
     with(NULL, df[, dist_end_to_deadline := NA])
   }
