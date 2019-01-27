2eaf131ef49cef3e92b7faa851cce334cf818bc8
m <scheer@freescience.de>
2019-01-27 08:09:14 +0100

    add unit-test

--- tests/testthat/test_calculate.R
+++ tests/testthat/test_calculate.R
@@ -215,8 +215,8 @@ d_in <- data.table::data.table(
   aborted = c(F, T, F, F),
   time_start = c(lubridate::ymd("2019-01-20"), lubridate::ymd("2019-01-26"), lubridate::ymd("2019-02-05"), lubridate::ymd("2019-01-05")),
   time_end   = c(lubridate::ymd("2019-01-23"), lubridate::ymd("2019-01-27"), lubridate::ymd("2019-02-15"), lubridate::ymd("2019-03-10")),
-  deadline   = c(lubridate::ymd("2019-01-23"), lubridate::ymd("2019-01-28"), lubridate::ymd("2019-02-14"), lubridate::ymd("2019-03-05")),
-  dist_end_to_deadline = c(0, 1, -1, -5),
+  deadline   = c(lubridate::as_date(NA), lubridate::as_date(NA), lubridate::ymd("2019-02-14"), lubridate::ymd("2019-03-05")),
+  dist_end_to_deadline = c(NA, NA, -1, -5),
   progress = c(100, 99, 100, 100)
 )
 d_out <- data.table::data.table(
@@ -225,8 +225,8 @@ d_out <- data.table::data.table(
   aborted = c(F, F),
   time_start = c(lubridate::ymd("2019-01-20"), lubridate::ymd("2019-01-05")),
   time_end   = c(lubridate::ymd("2019-01-23"), lubridate::ymd("2019-03-10")),
-  deadline   = c(lubridate::ymd("2019-01-23"), lubridate::ymd("2019-02-14")),
-  dist_end_to_deadline = c(0, -1),
+  deadline   = c(lubridate::as_date(NA), lubridate::ymd("2019-02-14")),
+  dist_end_to_deadline = c(NA, -1),
   progress = c(100, 100),
   task = c("A collapsed", "B collapsed"),
   waiting = c(F, F),
@@ -235,6 +235,24 @@ d_out <- data.table::data.table(
 test_that(
   "Collapsing when all tasks have status aborted or are complete",
   expect_identical(collapse_complete_projects(d_in), d_out)
+)
+
+
+d_in <- data.table::data.table(
+  project = c("A", "A", "B", "B"),
+  section = c("A::s1", "A::s1", "B::s1", "B::s1"),
+  aborted = c(T, F, F, F),
+  time_start = c(lubridate::ymd("2019-01-20"), lubridate::ymd("2019-01-26"), lubridate::ymd("2019-02-05"), lubridate::ymd("2019-01-05")),
+  time_end   = c(lubridate::ymd("2019-01-23"), lubridate::ymd("2019-01-27"), lubridate::ymd("2019-02-15"), lubridate::ymd("2019-03-10")),
+  deadline   = c(lubridate::ymd("2019-01-23"), lubridate::ymd("2019-01-28"), lubridate::ymd("2019-02-14"), lubridate::ymd("2019-03-05")),
+  dist_end_to_deadline = c(0, 1, -1, -5),
+  progress = c(100, 99, 100, 99)
+)
+test_that(
+  "No collapsing if section/Project is not complete", {
+    expect_identical(collapse_complete_projects(d_in), d_in)
+    expect_identical(collapse_complete_sections(d_in), d_in)
+  }
 )
 
 
